c40afdae9fe398ae79ef03b755ea1fad205a0666
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-05-27 19:43:29 +0900

    /04/040101-func-declaration-hoisting.tsを追加

---
+++ src/04/040101-func-declaration-hoisting.ts
@@ -0,0 +1,14 @@
+{
+  //関数宣言より前でも関数を呼び出しできる。
+  //関数宣言は巻き上げ(hoisting)される。
+  //このように関数宣言によって作られた関数はスコープに入った時から存在していることになる
+  console.log(range(5, 10));
+  //関数宣言で関数を作る
+  function range(min:number, max:number):number[] {
+    const result = [];
+    for (let i = min; i <= max; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+}
