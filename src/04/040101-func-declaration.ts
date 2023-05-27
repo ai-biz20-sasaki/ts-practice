e0fdd47142ad195764084707ebe9ca030e4fbab2
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-05-27 17:40:27 +0900

    /04/040101-func-declaration.tsを追加

---
+++ src/04/040101-func-declaration.ts
@@ -0,0 +1,11 @@
+{
+  //関数宣言で関数を作る
+  function range(min:number, max:number):number[] {
+    const result = [];
+    for (let i = min; i <= max; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  console.log(range(5, 10));
+}
