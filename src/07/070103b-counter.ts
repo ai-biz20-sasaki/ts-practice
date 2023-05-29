b689bb060bd54db95fbb7854f82409115d515a94
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-05-29 11:34:53 +0900

    add /07/070103b-xxx

---
+++ src/07/070103b-counter.ts
@@ -0,0 +1,6 @@
+let value =0;
+
+//関数宣言をdefaultエクスポートする
+export default function increment() {
+  return ++value;
+}
---
+++ src/07/070103b-index.ts
@@ -0,0 +1,6 @@
+//default exportされた式をuhyoAgeという変数名で受け取る
+import defaultExportedIncrement from "./070103b-counter.js";
+
+console.log(`カウンタの値は${defaultExportedIncrement()}です`);
+console.log(`カウンタの値は${defaultExportedIncrement()}です`);
+console.log(`カウンタの値は${defaultExportedIncrement()}です`);
