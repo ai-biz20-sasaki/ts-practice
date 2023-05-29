72e025a0fa701bf68edb6867d2aae50d78afa83d
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-05-29 10:55:11 +0900

    add /07/070102c-counter.ts,070102c-index.ts

---
+++ src/07/070102c-index.ts
@@ -0,0 +1,8 @@
+import { increment, getValue } from "./070102c-counter.js";
+
+increment();
+console.log(`カウンタの値は${getValue()}です`);
+increment();
+console.log(`カウンタの値は${getValue()}です`);
+increment();
+console.log(`カウンタの値は${getValue()}です`);
