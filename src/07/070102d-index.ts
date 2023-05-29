5b1af8b12f39498b4f9dbb93ebc0ec6763ea0930
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-05-29 10:57:38 +0900

    add /07/070102d-counter.ts,070102d-index.ts

---
+++ src/07/070102d-index.ts
@@ -0,0 +1,8 @@
+import { increment, value } from "./070102d-counter.js";
+
+increment();
+console.log(`カウンタの値は${value}です`);
+increment();
+console.log(`カウンタの値は${value}です`);
+increment();
+console.log(`カウンタの値は${value}です`);
