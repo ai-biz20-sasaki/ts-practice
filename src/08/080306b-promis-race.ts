853b67fbd5ffc52cb35b43b4b8a095c7f45f6492
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-05-30 18:41:02 +0900

    コメント修正

--- src/08/080306b-promis-race.ts
+++ src/08/080306b-promis-race.ts
@@ -1,6 +1,6 @@
 import { readFile } from "fs/promises";
 
-//3つのPromiseがすべて成功したら
+//3つのPromiseのうち最も早く成功または失敗したものの結果
 const p = Promise.race([
   readFile("./dist/08/080306a-foo.txt", "utf8"),
   readFile("./dist/08/080306a-bar.txt", "utf8"),
