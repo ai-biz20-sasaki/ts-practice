e98f3df3bae47495abdbc006bf9f0127d00a3ced
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-05-30 17:31:35 +0900

    add /08/080304b-xxx

---
+++ src/08/080304b-sleep-promis.ts
@@ -0,0 +1,11 @@
+{
+  const sleep = (duration: number) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(resolve, duration);
+    })
+  };
+
+  sleep(3000).then(() => {
+    console.log('3秒経ちました');
+  });
+}
