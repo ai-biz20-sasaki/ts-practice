e9ef68f0663385b7b6f8f7cee784b7b6b0f64ec3
ai-biz20-sasaki <ai.biz2.0.sasaki@gmail.com>
2023-06-01 17:30:12 +0900

    add /06/060203a-xxx

---
+++ src/06/060203a-union-literal-combination.ts
@@ -0,0 +1,9 @@
+export {};  //ファイルをモジュールにする
+
+function signNumber(type: "plus" | "minus") {
+  return type === "plus" ? 1 : -1;
+}
+
+console.log(signNumber("plus"));
+console.log(signNumber("minus"));
+//console.log(signNumber("uhyo")); //これはコンパイルエラーになる
